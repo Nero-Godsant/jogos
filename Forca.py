6528ebafc3162d78dd9f62f546a80ce187d28c29
João Pedro <jpdc2003@gmail.com>
2023-04-05 19:59:45 -0300

    Forca versao 1

--- Forca.py
+++ Forca.py
@@ -3,7 +3,25 @@ def jogar():
     print("Bem vindo no jogo de Forca!")
     print("*********************************")
 
+    palavra_secreta = "banana"
+
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+
+        chute = input("Qual letra? ")
+        chute = chute.strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            if(chute.upper() == letra.upper()):
+                print("Encontrei a letra {} na posição {}".format(letra, index))
+            index = index + 1
+
+        print("jogando...")
+
     print("Fim do jogo")
 
-    if(__name__ = "__main__")
-        jogar()
+if(__name__ == "__main__"):
+    jogar()
